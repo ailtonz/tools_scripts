637b3058cd36beaf6e583e41fb61594110e82b05
Ailton Zacarias <ailton@f4consult.com.br>
2020-04-02 15:36:46 -0300

    Add files via upload

---
+++ everday.bat
@@ -0,0 +1,23 @@
+REM Aplicativos
+
+idea64.exe
+notepadpp.exe
+for %%1 in (*.txt) do start notepadpp "%%1"
+
+REM Leitura
+
+explorer vire-o-jogo-com-spring-framework.pdf
+explorer Fundamentos-Do-Desenho-Orientado-a-Objeto-Com-UML.pdf
+explorer "Escrevendo Código Seguro.pdf"
+explorer "armazenando-dados-com-redis.pdf"
+
+REM Material de apoio
+
+cd apoio 
+for %%1 in (*.xlsx) do start excel "%%1"
+for %%1 in (*.jpg) do start mspaint "%%1"
+for %%1 in (*.pptx) do start powerpnt "%%1"
+for %%1 in (*.txt) do start notepadpp "%%1"
+for %%1 in (*.mm) do start FreeMind "%%1"
+explorer .
+pause
